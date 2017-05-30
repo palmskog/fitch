14720f936fd90d101c6e6b9c6c2acaa085fd66df
Karl Palmskog <palmskog@gmail.com>
2017-05-30 00:14:16 -0500

    add Travis files

---
+++ .travis-ci.sh
@@ -0,0 +1,15 @@
+set -ev
+
+opam init --yes --no-setup
+eval $(opam config env)
+
+opam repo add coq-released https://coq.inria.fr/opam/released
+opam repo add distributedcomponents-dev http://opam-dev.distributedcomponents.net
+
+opam pin add coq $COQ_VERSION --yes --verbose
+
+case $MODE in
+  *)
+    opam pin add fitch . --yes --verbose
+    ;;
+esac
